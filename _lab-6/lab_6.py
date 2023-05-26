c78ad08464847bfe2efba7f6e1c744b075036186
Falker34 <70699814+Falker34@users.noreply.github.com>
2023-05-26 10:02:20 +0300

    [6]

--- _lab-6/lab_6.py
+++ _lab-6/lab_6.py
@@ -3,16 +3,19 @@ import numpy as np
 import pathlib
 import tensorflow as tf
 import os
+from keras.callbacks import EarlyStopping
 
-data_dir = pathlib.Path("d:/work/2022-bigdata/_lab-6/movies_posters")
+
+
 img_height = 56
 img_width = 38
 batch_size = 32
-genres = pd.read_csv('d:/work/2022-bigdata/_lab-6/movies_dataset.csv')
+data_dir = pathlib.Path("C:/Users/NebutchadnezzaR/Documents/GitHub/2022-BigDat/_lab-6/movies_posters")
+genres = pd.read_csv('C:/Users/NebutchadnezzaR/Documents/GitHub/2022-BigDat/_lab-6/movies_dataset.csv')
 genres_set = set(genres['genres_list'])
 genres_dict = dict(zip(genres_set, range(len(genres_set))))
 
-
+#все картинки засовыываются датасет, потом он перемешивается в памяти, потому что у нас небольшой размер датасетов (ага, порядка 45 тысяч файлов)
 def load_imgs(data_dir=data_dir):
     image_count = len(list(data_dir.glob('*.jpg')))
     list_ds = tf.data.Dataset.list_files(str(data_dir/'*'), shuffle=False)
@@ -20,79 +23,90 @@ def load_imgs(data_dir=data_dir):
     return list_ds, image_count
 
 
-def get_label(file_path, genres=genres):
-    parts = tf.strings.split(file_path, sep=os.path.sep)
-    img_id = tf.strings.split(parts[-1], '.')[-2]
-    # g_list = list(genres[genres['id'] == img_id['genres_list']])
-    # g_indeces = [genres_dict[x] for x in g_list]
-    # меняем пока на год выпуска
-    return int(genres.loc[genres['id'] == img_id, 'release_date'].iloc[0][:4])
-
-
-def decode_img(img, img_height=img_height, img_width=img_width):
-    img = tf.io.decode_jpeg(img, channels=3)
-    return tf.image.resize(img, [img_height, img_width])
-
-
-def process_path(file_path):
-    label = get_label(file_path)
-    img = tf.io.read_file(file_path)
-    img = decode_img(img)
-    return img, label
-
-
+#применяю ограничения
+#мы создаём датасет для обучения,
 train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+        label_mode="categorical")
+    #мы создаём датасет для проверки
 val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+        label_mode="categorical")
+    #это уже идут слои нашей нейросетки(почему такие и почему столько - предстоит узнать)
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(input_shape=(img_height, img_width, 3),
-                           filters=2128, kernel_size=batch_size,
-                           padding='same', activation='sigmoid'),
-    tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
-            padding='valid', data_format='channels_last'),
-    tf.keras.layers.Conv2D(filters=768, kernel_size=batch_size,
-                           padding='same', activation='sigmoid'),
-    tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
-            padding='valid', data_format='channels_last'),
-    tf.keras.layers.Conv2D(filters=64, kernel_size=batch_size,
-                           padding='same', activation='sigmoid'),
-    tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
-            padding='valid', data_format='channels_last'),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(64, activation='softmax'),
-    tf.keras.layers.Dense(len(genres_set))
-])
+        #Полносвязный слой
+        #я не хочу два ляма параметров, поэтому сейчас порежу фильтры этот был 2128
+        tf.keras.layers.Conv2D(input_shape=(img_height, img_width, 3),
+                filters=50, kernel_size=batch_size,
+                padding='same', activation='relu', ),
+        #Слой от переобучения
+        tf.keras.layers.BatchNormalization(),
+        #Слой свёртки
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
+                padding='valid', data_format='channels_last'),
+        #Слой от переобучения
+        tf.keras.layers.Dropout(0.25),
 
-model.compile(optimizer='adam', loss='BinaryCrossentropy',
-              metrics=['accuracy'])
+        #я не хочу два ляма параметров, поэтому сейчас порежу фильтры этот был 768
+        tf.keras.layers.Conv2D(filters=10, kernel_size=batch_size,
+                padding='same', activation='relu',),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
+                padding='valid', data_format='channels_last'),
+        tf.keras.layers.Dropout(0.25),
+        # этот был 64
+        tf.keras.layers.Conv2D(filters=10, kernel_size=batch_size,
+                padding='same', activation='relu',),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=None,
+                padding='valid', data_format='channels_last'),
+        
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
 
-# Define the checkpoint callback that saves the model's weights at every epoch
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(20, activation='softmax'),
+
+        #tf.keras.layers.Dense(len(genres_set))
+    ])
+    # начинаем обучение модели алгоритм оптимизации Адам, функция потерь "Бинарная кроссэнтропия", метрика будет показывать долю правильных ответов модели
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+                metrics=['accuracy'])
+    # тут сложно что-то понять, вроде как в конце каждого цикла происходит сохранение весов модели...
 epoch = 0
 PATH = f'checkpoint_{epoch}.ckpt'
+    #подключаю Early_stopping чтобы избежать переобучения
+early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(
-                             filepath=PATH,
-                             save_weights_only=True, # If False, saves the full model
-                             save_freq='epoch')
-
+                                filepath=PATH,
+                                save_weights_only=True, # If False, saves the full model
+                                save_freq='epoch')
+    #выводит сводку нейрокки в терминале
 model.summary()
+    #требует ввода чего-нибудь перед началом обучения нейронки
 input()
 model.fit(train_ds,
-          validation_data=val_ds,
-          validation_freq=[2, 5, 9],
-          epochs=9,
-          callbacks=[cp_callback])
+            validation_data=val_ds,
+            validation_freq=[2, 5, 9],
+            epochs=9,
+            callbacks=[cp_callback, early_stopping],
+            use_multiprocessing=True)
+
+test_loss, test_accuracy = model.evaluate(val_ds)
+print('Test accuracy:', test_accuracy)
